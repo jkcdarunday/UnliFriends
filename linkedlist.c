4930fd8846ab0dfda9d1b0d80ef677f4a0458f7d
Keith Certeza Darunday <jkcdarunday@gmail.com>
2013-12-29 13:56:52 +0800

    Added base files and structure definitions.

---
+++ linkedlist.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct nodetag node;
+struct nodetag{
+    node *prev, *next; //Pointers that points to the previous and the next nodes
+    void *data; //Points to the data the the node is refering to. You'll have to typecast the data by using something like *(int*)node->data
+};
+
+void insert_head(){
+}
+
+void insert_tail(){
+}
+
+void insert_sort(){
+} 
+
+void delete(){
+}
