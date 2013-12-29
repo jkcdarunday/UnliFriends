1ea36874d6d4d9a90637dc91c0d59a52f8e43c41
Keith Certeza Darunday <jkcdarunday@gmail.com>
2013-12-29 14:58:48 +0800

    Added functions that create new lists and nodes

--- linkedlist.c
+++ linkedlist.c
@@ -1,4 +1,23 @@
 #include "unlifriends.h"
+/*
+ * Doubly Linked List with 2 Dummy Nodes
+*/
+
+
+node* new_node(){
+    node *tmp = malloc(sizeof(node));
+    tmp->data=NULL;
+    return tmp;
+}
+
+list* new_list(){
+    list *tmp = malloc(sizeof(list));
+    tmp->head=new_node();
+    tmp->tail=new_node();
+    tmp->head->next=tmp->tail;
+    tmp->tail->next=tmp->head;
+    return tmp;
+}
 
 void insert_head(){
 }
