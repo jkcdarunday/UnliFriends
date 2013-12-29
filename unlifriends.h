b3eb393d17ddc77e75136b14287bbd589ade0d4a
Keith Certeza Darunday <jkcdarunday@gmail.com>
2013-12-29 14:48:58 +0800

    Added "student" data structure

--- unlifriends.h
+++ unlifriends.h
@@ -5,10 +5,16 @@
 #include <stdlib.h>
 
 typedef struct nodetag node;
+typedef struct studenttag student;
+
 struct nodetag{
     node *prev, *next; //Pointers that points to the previous and the next nodes
     void *data; //Points to the data the the node is refering to. You'll have to typecast the data by using something like *(int*)node->data
 };
+
+struct studenttag{
+    char *name, *course, *snum;
+}
 
 void insert_head();
 void insert_tail();
