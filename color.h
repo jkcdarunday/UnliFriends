bd68be3471d72994df0f21e91a577cba0793dc91
Akira-Shirase <ferrielm@gmail.com>
2014-01-03 17:39:03 +0800

    Added color.h header file

---
+++ color.h
@@ -0,0 +1,33 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#define CLR  "\x1b[m"
+
+#define FCLR  "\x1b[39m"
+#define RED  "\x1b[31m"
+#define GRN  "\x1b[32m"
+#define YEL  "\x1b[33m"
+#define BLU  "\x1b[34m"
+#define MAG  "\x1b[35m"
+#define CYN  "\x1b[36m"
+#define WHT  "\x1b[37m"
+
+#define BCLR  "\x1b[49m"
+#define BRED  "\x1b[41m"
+#define BGRN  "\x1b[42m"
+#define BYEL  "\x1b[43m"
+#define BBLU  "\x1b[44m"
+#define BMAG  "\x1b[45m"
+#define BCYN  "\x1b[46m"
+#define BWHT  "\x1b[47m"
+
+#define ICLR  "\x1b[0m"
+#define ITA "\x1b[3m"
+#define UND "\x1b[4m"
+#define BNK "\x1b[5m"
+#define BRT "\x1b[1m"
+#define DIM "\x1b[2m"
+#define INV "\x1b[7m"
+
+
+#endif // COLOR_H
